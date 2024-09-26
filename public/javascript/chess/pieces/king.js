a8dc593720496ab607e02884f544d429b4f9d5a8
shailendra-jurel <shailendrajurel001@gmail.com>
2024-09-26 19:00:15 +0530

    Implemented  the  King  logic  but  have to add more

--- public/javascript/chess/pieces/king.js
+++ public/javascript/chess/pieces/king.js
@@ -6,6 +6,59 @@ var King = function(config){
 
 
 King.prototype = new Piece({});
-King.prototype.move = function(newPosition){
+
+King.prototype.isValidMove = function(newPosition){
+
+    let curCol  =  this.position.charAt(0);
+    let curRow = this.position.charAt(1);
+
+    let newCol = newPosition.col;
+    let newRow = newPosition.row;
+//  
+    let colDiff = Math.abs(curCol.charCodeAt(0) - newCol.charCodeAt(0));
+    let rowDiff = Math.abs(curRow - newRow);
+
+    let targetPiece = this.board.getPiece(newPosition);
+
+    if(colDiff <= 1 && rowDiff <= 1){
+        if(targetPiece == null || targetPiece.color != this.color){
+            if(targetPiece && targetPiece.color != this.color){
+                
+                targetPiece.kill(targetPiece);
+            }
+
+            return true;
+        }
+    }
+//    I have  a doubt here   figure out later
+    console.warn("Invalid move for King");
+
+    return false;
+
+
+}
+
+
+
+
+
+
+King.prototype.moveTo = function(newPosition){
+
+    if(this.isValidMove(newPosition)){
+        this.position = newPosition.col + newPosition.row;
+        // return true;
+
+        this.render();
+
+
+    }
+    
+
+    else{
+        this.board.invalidMove();
+    }
+
+
 
 }
