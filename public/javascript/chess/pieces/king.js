5c77493b513281d1340eb9c4b4aa7965ab805f75
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2024-09-26 23:36:42 +0530

    Added the invalid move

--- public/javascript/chess/pieces/king.js
+++ public/javascript/chess/pieces/king.js
@@ -17,3 +17,28 @@ King.prototype.moveTo = function(newPosition){
         this.board.invalidMove();
     }
 };
+
+King.prototype.isValid = function(newPosition){
+    let curCol = this.position.charAt(0);
+    let curRow = parseInt(this.position.charAt(1));
+
+    let targetCol = newPosition.col;
+    let targetRow = newPosition.row;
+
+    let colDiff = Math.abs(targetCol.charCodeAt(0) - curCol.charCodeAt(0));
+    let rowDiff = Math.abs(targetRow - curRow);
+
+    let targetPiece = this.board.getPieceAt(newPosition);
+
+    if (colDiff <= 1 && rowDiff <= 1) {
+        if (!targetPiece || targetPiece.color !== this.color) {
+            if (targetPiece && targetPiece.color !== this.color) {
+                // Capture the opponent's piece
+                targetPiece.kill(targetPiece);
+            }
+            return true;
+        }
+    }
+    console.warn("Invalid move for king");
+    return false;
+};
