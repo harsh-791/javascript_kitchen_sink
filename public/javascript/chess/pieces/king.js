217d6613e838144d6db0f0fe39322bf44028b5dd
shailendra-jurel <shailendrajurel001@gmail.com>
2024-09-26 21:05:39 +0530

    Adding the move to method now working properly

--- public/javascript/chess/pieces/king.js
+++ public/javascript/chess/pieces/king.js
@@ -2,63 +2,42 @@ var King = function(config){
     this.type = 'king';
     this.constructor(config);
 };
-
-
-
 King.prototype = new Piece({});
 
-King.prototype.isValidMove = function(newPosition){
+King.prototype.isValid = function(newPosition){
+    let curCol = this.position.charAt(0);
+    let curRow = parseInt(this.position.charAt(1));
 
-    let curCol  =  this.position.charAt(0);
-    let curRow = this.position.charAt(1);
+    let targetCol = newPosition.col;
+    let targetRow = newPosition.row;
 
-    let newCol = newPosition.col;
-    let newRow = newPosition.row;
-//  
-    let colDiff = Math.abs(curCol.charCodeAt(0) - newCol.charCodeAt(0));
-    let rowDiff = Math.abs(curRow - newRow);
+    let colDiff = Math.abs(targetCol.charCodeAt(0) - curCol.charCodeAt(0));
+    let rowDiff = Math.abs(targetRow - curRow);
 
-    let targetPiece = this.board.getPiece(newPosition);
+    let targetPiece = this.board.getPieceAt(newPosition);
 
-    if(colDiff <= 1 && rowDiff <= 1){
-        if(targetPiece == null || targetPiece.color != this.color){
-            if(targetPiece && targetPiece.color != this.color){
-                
+    if (colDiff <= 1 && rowDiff <= 1) {
+        if (!targetPiece || targetPiece.color !== this.color) {
+            if (targetPiece && targetPiece.color !== this.color) {
+                // Capture the opponent's piece
                 targetPiece.kill(targetPiece);
             }
-
             return true;
         }
     }
-//    I have  a doubt here   figure out later
-    console.warn("Invalid move for King");
-
+    console.warn("Invalid move for king");
     return false;
-
-
-}
-
-
-
-
-
+};
 
 King.prototype.moveTo = function(newPosition){
-
-    if(this.isValidMove(newPosition)){
+    // Check if the move is valid
+    if(this.isValid(newPosition)){
+        // Update the King's position
         this.position = newPosition.col + newPosition.row;
-        // return true;
-
+        
         this.render();
-
-
-    }
-    
-
-    else{
+        this.board.switchPlayer();
+    } else {
         this.board.invalidMove();
     }
-
-
-
-}
+};
